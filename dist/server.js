4828ec0826473ec01951c1d2d4aa34b84ac987bc
DanielSiilva <daniellsousa0614@gmail.com>
2024-09-10 13:42:42 -0300

    feat: add new version cors

--- dist/server.js
+++ dist/server.js
@@ -5,10 +5,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const cors_1 = __importDefault(require("cors"));
 const postHandler_1 = require("./routes/postHandler");
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = process.env.PORT || 3000;
+// Use o CORS para aceitar requisições de qualquer origem
+app.use((0, cors_1.default)({
+    origin: "*", // Aceitar requisições de qualquer lugar
+    methods: ["GET", "POST", "PUT", "DELETE"], // Métodos permitidos
+    allowedHeaders: ["Content-Type", "Authorization"], // Cabeçalhos permitidos
+}));
 app.use(express_1.default.json());
 app.post("/api/chat", postHandler_1.postHandler);
 app.listen(port, () => {
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import { postHandler } from "./routes/postHandler";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+// Use o CORS para aceitar requisições de qualquer origem
+app.use(
+  cors({
+    origin: "*", // Aceitar requisições de qualquer lugar
+    methods: ["GET", "POST", "PUT", "DELETE"], // Métodos permitidos
+    allowedHeaders: ["Content-Type", "Authorization"], // Cabeçalhos permitidos
+  })
+);
 
 app.use(express.json());
 
